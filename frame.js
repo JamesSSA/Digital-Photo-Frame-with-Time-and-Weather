4c907928b33d0a7e72d145bc2facc2c26a334c38
JamesSSA <james.br.gibson@gmail.com>
2020-01-08 15:09:53 +1100

    Updated days array in frame.js setDate() ln71 so that sunday is the zeroth element (because javascript).

--- frame.js
+++ frame.js
@@ -68,7 +68,7 @@ function setDate() {
   let day = today.getDay();
   let month = today.getMonth();
 
-  let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   let months = ["January", "Feburary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   day = days[day];
   month = months[month];
